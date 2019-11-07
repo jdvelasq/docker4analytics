05cdfcd37db59f4d181652f33fad5e7cdd0298c5
jdvelasq <jdvelasq@unal.edu.co>
2019-11-07 16:48:58 -0500

    remove history sever

--- hadoop/bin/hdp-start.sh
+++ hadoop/bin/hdp-start.sh
@@ -5,11 +5,13 @@ rm -rf /tmp/hadoop-root/dfs/name
 hdfs namenode -format
 bash /usr/local/hadoop/sbin/start-dfs.sh
 bash start-yarn.sh
-mr-jobhistory-daemon.sh start historyserver
 hdfs dfs -mkdir       /tmp
 hdfs dfs -chmod 777   /tmp
 hdfs dfs -mkdir       /user
 hdfs dfs -mkdir       /user/root
+
+## Si se agrega para Apache Pig hadoop muere en la corrida
+## mr-jobhistory-daemon.sh start historyserver
 
 echo 
 echo ======================================
