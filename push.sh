da1e96399da0c50cb50c9a1ed830f2736dec3488
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2020-10-29 21:06:49 -0500

    modificacion estructura de directorios

---
+++ push.sh
@@ -0,0 +1,14 @@
+docker push jdvelasq/python3:vscode-minimal
+docker push jdvelasq/python3:colab
+docker push jdvelasq/hadoop:2.8.5-pseudo
+docker push jdvelasq/hadoop:2.8.5-standalone
+docker push jdvelasq/hive:2.3.7-pseudo
+docker push jdvelasq/hive:2.3.7-standalone
+docker push jdvelasq/pig:0.17.0-pseudo
+docker push jdvelasq/pig:0.17.0-standalone
+docker push jdvelasq/spark:2.4.7
+docker push jdvelasq/pyspark:2.4.7-pseudo
+docker push jdvelasq/pyspark:2.4.7-standalone
+
+# docker push jdvelasq/mahout:0.13.0-pseudo
+# docker push jdvelasq/mahout:0.13.0-standalone
