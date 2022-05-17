942c41fd495bfe7e73af4a99ce5077e827613f6c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-05-16 23:10:29 -0500

    Create hive-start.sh

---
+++ hive/bin/hive-start.sh
@@ -0,0 +1,10 @@
+hdfs dfs -mkdir       /tmp/hive
+hdfs dfs -mkdir       /user/
+hdfs dfs -mkdir       /user/hive
+hdfs dfs -mkdir       /user/hive/warehouse
+
+hdfs dfs -chmod 777   /tmp/hive
+hdfs dfs -chmod 777   /user/hive/
+hdfs dfs -chmod 777   /user/hive/warehouse
+
+schematool -dbType derby -initSchema
