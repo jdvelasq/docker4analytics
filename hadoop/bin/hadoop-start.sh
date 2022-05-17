c1c96724df3cc8bb287e15d93fdc83d7bb565560
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-05-16 21:29:47 -0500

    Update hadoop-start.sh

--- hadoop/bin/hadoop-start.sh
+++ hadoop/bin/hadoop-start.sh
@@ -26,12 +26,3 @@ echo "     http://127.0.0.1:8088/"
 echo 
 echo ======================================
 echo
-
-# echo To run Zeppelin:
-# echo
-# echo "   bash zeppelin-daemon.sh start"
-# echo "   http://127.0.0.1:8090/"
-# echo "   bash zeppelin-daemon.sh stop"
-# echo 
-# echo ======================================
-# echo
