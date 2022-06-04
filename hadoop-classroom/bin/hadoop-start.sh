bf95ba57d05299814fd72f401276acffe50f5327
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-04 14:43:29 -0500

    Create hadoop-start.sh

---
+++ hadoop-classroom/bin/hadoop-start.sh
@@ -0,0 +1,28 @@
+# /etc/init.d/ssh start
+
+rm -rf /tmp/hadoop-root/dfs/name
+
+hdfs namenode -format
+bash /opt/hadoop/sbin/start-dfs.sh
+bash start-yarn.sh
+hdfs dfs -mkdir       /tmp
+hdfs dfs -chmod 777   /tmp
+hdfs dfs -mkdir       /user
+hdfs dfs -mkdir       /user/root
+
+# Si se agrega para Apache Pig hadoop muere en la corrida
+# mr-jobhistory-daemon.sh start historyserver
+
+echo 
+echo ======================================
+echo
+echo Hadoop NameNode at:
+echo 
+echo "    http://127.0.0.1:50070/"
+echo
+echo Yarn ResourceManager at:
+echo
+echo "     http://127.0.0.1:8088/"
+echo 
+echo ======================================
+echo
